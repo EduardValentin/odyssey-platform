12db58341fe8b2bdf01d95fdc380dc4c85fb1a9c
Eduard Valentin <eduard.valentin1996@gmail.com>
2024-05-22 19:04:31 +0300

    add netlify and astro db studio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
 
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), db()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify(),
 });
+

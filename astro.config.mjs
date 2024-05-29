cea9ff0fc9e3d1404d69b12ec34e3dc8f4ecea2f
Eduard Valentin <eduard.valentin1996@gmail.com>
2024-05-29 14:18:16 +0300

    added webvitals

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
-
 import netlify from "@astrojs/netlify";
+
+import webVitals from "@astrojs/web-vitals";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db()],
+  integrations: [tailwind(), db(), webVitals()],
   output: "server",
-  adapter: netlify(),
+  adapter: netlify()
 });
-

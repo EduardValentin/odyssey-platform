5fc5091364d858e81e0ca3898888864d5a4d6512
Eduard Valentin <eduard.valentin1996@gmail.com>
2024-07-25 17:23:16 +0300

    content changes and added overview and redid curriculum page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
 import netlify from "@astrojs/netlify";
-
 import webVitals from "@astrojs/web-vitals";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db(), webVitals()],
+  integrations: [tailwind(), db(), webVitals(), mdx()],
   output: "server",
   adapter: netlify()
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,20 @@
 import { defineCollection, z } from "astro:content";
 
-const previewsCollection = defineCollection({
-	schema: () => z.object({
-		title: z.string(),
-		index: z.number(),
-	})
-
+const curriculum = defineCollection({
+  type: "data",
+  schema: () =>
+    z.object({
+      title: z.string(),
+      index: z.number(),
+      items: z.array(
+        z.object({
+          name: z.string(),
+          content: z.string(),
+        }),
+      ),
+    }),
 });
 
 export const collections = {
-	previews: previewsCollection,
+  curriculum,
 };
